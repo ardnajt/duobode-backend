649d462f3bced6adbeb756f9f9112291d255545a
ardnajt <199881779+ardnajt@users.noreply.github.com>
2025-05-22 17:08:26 +0800

    Finding/creating User on Google callback

--- src/plugins/oauth2.plugin.ts
+++ src/plugins/oauth2.plugin.ts
@@ -1,8 +1,12 @@
 import fastifyPlugin from 'fastify-plugin';
 import fastifyOauth2 from '@fastify/oauth2';
-import { Type } from '@sinclair/typebox';
+import axios from 'axios';
+import { initORM } from '@orm';
+import { User } from '@modules/user/user.entity';
 
 const fastifyOauth2Plugin = fastifyPlugin(async app => {
+	const db = await initORM();
+
 	app.register(fastifyOauth2, {
 		name: 'GoogleOAuth2',
 		scope: ['profile', 'email'],
@@ -11,27 +15,33 @@ const fastifyOauth2Plugin = fastifyPlugin(async app => {
 				id: process.env.API_OAUTH2_GOOGLE_CLIENT,
 				secret: process.env.API_OAUTH2_GOOGLE_SECRET
 			},
-			auth: fastifyOauth2.GOOGLE_CONFIGURATION,
+			// auth: fastifyOauth2.GOOGLE_CONFIGURATION,
+		},
+		discovery: {
+			issuer: 'https://accounts.google.com'
 		},
 		startRedirectPath: '/oauth2/google',
 		callbackUri: `http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}${process.env.API_OAUTH2_GOOGLE_CALLBACK_URI}`
 	});
 
-	// Example of reading the access token.
-	app.get('/', {
-		schema: {
-			querystring: Type.Object({
-				access_token: Type.String()
-			})
-		}
-	}, async (req, res) => {
-		res.send(req.query);
-	});
-
 	// Endpoint to retrieve token on server-side on successful login.
 	app.get(process.env.API_OAUTH2_GOOGLE_CALLBACK_URI, async (req, res) => {
-		const { token } = await app.GoogleOAuth2.getAccessTokenFromAuthorizationCodeFlow(req);
-		res.redirect(`http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/?access_token=${token.access_token}`);
+		const em = db.em.fork();
+
+		const { token: accessToken } = await app.GoogleOAuth2.getAccessTokenFromAuthorizationCodeFlow(req);
+		const info = await app.GoogleOAuth2.userinfo(accessToken.access_token) as { sub: string, email: string, picture: string, name: string };
+
+		let created = false;
+		let user = await em.findOne(User, { social: { googleId: info.sub }});
+		if (!user) {
+			created = true;
+			user = new User(info.name, info.email);
+			user.social = { googleId: info.sub };
+			await em.persistAndFlush(user);
+		}
+
+		const token = user.generateToken(app);
+		return created ? `You have created a new account with ${token}.` : `You have logged in with ${token}.`;
 	});
 });
 
