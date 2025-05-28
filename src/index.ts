b34b981970fc82c0babdfdb593e28c4c7b842e8f
ardnajt <199881779+ardnajt@users.noreply.github.com>
2025-05-29 01:28:26 +0800

    Temporarily removed host to see if it solves deployment issue

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
 	await app.ready();
 	app.swagger();
 
-	app.listen({ host: process.env.SERVER_HOST ?? '0.0.0.0', port: Number(process.env.SERVER_PORT) ?? 3000 }, (err, address) => {
+	app.listen({ port: Number(process.env.SERVER_PORT) ?? 3000 }, (err, address) => {
 		if (err) {
 			app.log.error(err);
 			process.exit(1);
